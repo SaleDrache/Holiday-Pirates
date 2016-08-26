7763cc1855532390d77995a7b5577203db227cd0
Aleksandar Stojadinovic <astojadinovicsd@gmail.com>
2016-08-26 23:03:28 +0200

    yeoman init

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('holidaypirates')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController() {
+    var vm = this;
+
+
+    activate();
+
+    function activate() {
+
+    }
+
+  }
+})();
